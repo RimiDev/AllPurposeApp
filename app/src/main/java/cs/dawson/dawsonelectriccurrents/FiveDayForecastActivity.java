3ec679d8658a28e8de3da328fbf7c4c52846c1ca
Maxime Lacasse <maximelacasse@Maximes-MBP.local>
2017-11-25 09:32:28 -0500

    HTTPresponse is working! I am receiving the JSON file, now i have to parse it and place it into the activity

--- app/src/main/java/cs/dawson/dawsonelectriccurrents/FiveDayForecastActivity.java
+++ app/src/main/java/cs/dawson/dawsonelectriccurrents/FiveDayForecastActivity.java
@@ -1,5 +1,6 @@
 package cs.dawson.dawsonelectriccurrents;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,13 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -29,6 +34,8 @@ public class FiveDayForecastActivity extends MenuActivity {
 
     String WEATHER_API_KEY = "..";
     TextView JSONresponse;
+    private String api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d00d22f8b4783f4e387c4cbad258470f";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +43,16 @@ public class FiveDayForecastActivity extends MenuActivity {
         setContentView(R.layout.activity_fiveday_forecast);
 
 
+        logIt("I am here");
+
         Button btnHit = (Button) findViewById(R.id.btnHit);
         JSONresponse = (TextView) findViewById(R.id.JSONresponse);
 
         btnHit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ReadJSONTask jsonTask = new ReadJSONTask();
-                jsonTask.execute("http://puppygifs.tumblr.com/api/read/json");
-
-                d00d22f8b4783f4e387c4cbad258470f
+                MyAsync myAsync = new MyAsync();
+                myAsync.execute();
             }
         });
 
@@ -63,35 +70,84 @@ public class FiveDayForecastActivity extends MenuActivity {
     }
 
 
-    public class ReadJSONTask extends AsyncTask<String, Void, String> {
+    public class MyAsync extends AsyncTask<Void, Void, String>{
+
         @Override
-        protected String doInBackground(String... url) {
-            String response = null;
-            String adres = url[0];
+        protected void onPreExecute() {
+            logIt("onPreExecute");
+            super.onPreExecute();
+        }
 
-            URL url_HTTPRequest = null;
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = null;
             try {
-                url_HTTPRequest = new URL(adres);
-                response = String.valueOf(url_HTTPRequest);
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d00d22f8b4783f4e387c4cbad258470f");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-            } catch (MalformedURLException e) {
-                Log.e("URL ERROR", "problem z url");
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                result = inputStreamToString(in);
+                logIt("result: " + result);
             } catch (Exception e) {
-                Log.e("URL ERROR", "problem z transferem");
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                logIt("ResultInOnPostExecute: "+ s);
+                JSONObject jsonObject = new JSONObject(s);
+//                JSONArray jsonArray = jsonObject.getJSONArray("contacts");
+//                for (int i = 0; i < jsonArray.length(); i++) {
+//                    String id = jsonArray.getJSONObject(i).getString("id");
+//                    String name = jsonArray.getJSONObject(i).getString("name");
+//                    String email = jsonArray.getJSONObject(i).getString("email");
+//                    String mobile = jsonArray.getJSONObject(i).getJSONObject("phone").getString("mobile");
+
+//                    JSONresponse.append(id + "\n");
+//                    JSONresponse.append(name + "\n");
+//                    JSONresponse.append(email + "\n");
+//                    JSONresponse.append(mobile + "\n\n");
+
+                   
+
+                //}
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
-            return response;
         }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d("mylog", "result= " + result);
-        }
-
-
     }
 
-    public void httpJSONreceive() throws MalformedURLException {
+
+    private String inputStreamToString(InputStream is) {
+        String rLine = "";
+        StringBuilder answer = new StringBuilder();
+
+        InputStreamReader isr = new InputStreamReader(is);
+
+        BufferedReader rd = new BufferedReader(isr);
+
+        try {
+            while ((rLine = rd.readLine()) != null) {
+                answer.append(rLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return answer.toString();
+    }
+
+
+
+
+
+
+    public String httpJSONreceive() throws MalformedURLException {
         URL url = new URL("http://www.android.com/");
         HttpURLConnection urlConnection = null;
         try {
@@ -103,7 +159,7 @@ public class FiveDayForecastActivity extends MenuActivity {
         } finally {
             urlConnection.disconnect();
         }
-
+        return null;
     } // end of httpJSONreceive
 
     private String readStream(InputStream is) {
