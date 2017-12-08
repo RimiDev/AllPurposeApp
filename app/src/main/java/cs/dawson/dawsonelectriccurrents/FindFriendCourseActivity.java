b35864d2fff393c1a6980e548bd35061323e7156
aciotola <alessandromciotola@gmail.com>
2017-12-08 18:18:59 -0500

    FindFriendActivity done and tested with hard coded values, waiting for password from db to complete, implemented an arrayList for friends
    Able to get all friends from the JSON response and display the friends in a listview. Began the FindFriendCourseActivity ()Getting the date and time.

--- app/src/main/java/cs/dawson/dawsonelectriccurrents/FindFriendCourseActivity.java
+++ app/src/main/java/cs/dawson/dawsonelectriccurrents/FindFriendCourseActivity.java
@@ -1,7 +1,9 @@
 package cs.dawson.dawsonelectriccurrents;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,11 +14,33 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FindFriendCourseActivity extends MenuActivity
 {
     private boolean isAvailable = false;
+    private TextView friendAvailable;
+    private String email;
+    private int day;
+    private String localTime;
+    private String dayOfWeek;
+    private String course;
+    private String section;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,14 +48,84 @@ public class FindFriendCourseActivity extends MenuActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friend_course);
 
-        //String email = getIntent().getExtras().getString("friendEmail");
+        email = getIntent().getExtras().getString("friendEmail");
 
-        TextView friendAvailable = (TextView) findViewById(R.id.availableView);
+        friendAvailable = (TextView) findViewById(R.id.availableView);
 
-        if(!isAvailable)
-            friendAvailable.setText(R.string.notAvailableText);
-        else
-            friendAvailable.setText(R.string.availableText + " Course: " + " Section: ");
+        Calendar cal = Calendar.getInstance();
+        Date currentLocalTime = cal.getTime();
+        DateFormat time = new SimpleDateFormat("HH:mm a");
+        localTime = time.format(currentLocalTime);
+
+        DateFormat date = new SimpleDateFormat("dd");
+        day = cal.get(Calendar.DAY_OF_WEEK);
+        dayOfWeek = date.format(day);
+
+        Log.d("LINE", "time: " + time + " dayOfWeek: " + dayOfWeek + " " + email);
+
+
+        FriendsAsyncTask hello = new FriendsAsyncTask();
+        hello.execute();
+    }
+
+    private class FriendsAsyncTask extends AsyncTask<String, Void, ArrayList<String>>
+    {
+        @Override
+        protected ArrayList<String> doInBackground(String... urls)
+        {
+            //String friendUrl = "http://dawsonfriendfinder2017.herokuapp.com/api/api/whereisfriends?email=alessandromciotola@gmail.com&password=shadowx15" +
+                    //"&friendemail=" + email + "&day=" + day + "&time=" + localTime;
+            String friendUrl = "http://dawsonfriendfinder2017.herokuapp.com/api/api/whereisfriend?" +
+                    "email=alessandromciotola@gmail.com&password=shadowx15" +
+                    "&friendemail=" + email + "&day=monday&time=1110";
+            try
+            {
+                URL url = new URL(friendUrl);
+                HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                boolean isError = httpConnection.getResponseCode() >= 400;
+                InputStream is = isError ? new BufferedInputStream(httpConnection.getErrorStream()) :
+                        new BufferedInputStream(httpConnection.getInputStream());
+
+                String result = "";
+                StringBuilder returningResults = new StringBuilder();
+                InputStreamReader inStreamResults = new InputStreamReader(is);
+                BufferedReader readBuffer = new BufferedReader(inStreamResults);
+
+                while ((result = readBuffer.readLine()) != null)
+                    returningResults.append(result);
+
+                Log.d("RETURNING", "doInBackground: " + returningResults.toString());
+
+                JSONArray jsonObject = new JSONArray(returningResults.toString());
+                    course = jsonObject.getJSONObject(0).getString("course");
+                    section = jsonObject.getJSONObject(0).getString("section");
+                    Log.d("LINE", "doInBackground: " + course + " " + section);
+
+                if(course != null || !course.equals("") || section != null || !section.equals(""))
+                    isAvailable = true;
+
+            }
+            catch (IOException | JSONException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> s)
+        {
+            super.onPostExecute(s);
+
+            if(!isAvailable)
+                friendAvailable.setText(R.string.notAvailableText);
+            else
+                friendAvailable.setText(R.string.availableText + " Course: " + " Section: ");
+
+        }
     }
 
 
