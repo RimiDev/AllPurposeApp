ceba69c784365d507e41276a36bdf8e78118fe71
aCiotola <alessandromciotola@gmail.com>
2017-12-09 19:12:41 -0500

    Add changes to the CalendarActivity with error checking and bundles

--- app/src/main/java/cs/dawson/dawsonelectriccurrents/CalendarActivity.java
+++ app/src/main/java/cs/dawson/dawsonelectriccurrents/CalendarActivity.java
@@ -10,7 +10,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.provider.CalendarContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,17 +19,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-import org.w3c.dom.Text;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
 
+/**
+ * This is an activity used to add an event to the user's calendar.
+ * The use can enter the title of the event and pick a date and start and end time.
+ * This event will be added to the user's default calendar on their device.
+ *
+ * For the date picker and time picker dialogs, I got some code from this github repository:
+ * https://github.com/mitchtabian/DatePickerDialog
+ *
+ * @author Hannah Ly
+ * @version 09/12/2017
+ */
 public class CalendarActivity extends MenuActivity
 {
     private static final String EVENTCREATED = "The event was created.";
     private static final String TIMEZONE  = "America/Montreal";
-	private EditText etTitle;
+    private EditText etTitle;
     private TextView datePickerTV;
     private TextView startTimePickerTV;
     private TextView endTimePickerTV;
@@ -45,6 +51,11 @@ public class CalendarActivity extends MenuActivity
     private int endHour;
     private int endMinute;
 
+    /**
+     * First method called when the activity is launched.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,8 +69,36 @@ public class CalendarActivity extends MenuActivity
         setDatePickerTVListener();
         setStartTimePickerTVListener();
         setEndTimePickerTVListener();
+
+        if(savedInstanceState != null){
+            etTitle.setText(savedInstanceState.getString("title"));
+            datePickerTV.setText(savedInstanceState.getString("date"));
+            startTimePickerTV.setText(savedInstanceState.getString("startTime"));
+            endTimePickerTV.setText(savedInstanceState.getString("endTime"));
+        }
     }
 
+    /**
+     * Method used to save the state of the textviews in a bundle.
+     *
+     * @param outSate
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outSate){
+        super.onSaveInstanceState(outSate);
+        outSate.putString("title", etTitle.getText().toString());
+        outSate.putString("date", datePickerTV.getText().toString());
+        outSate.putString("startTime", startTimePickerTV.getText().toString());
+        outSate.putString("endTime", endTimePickerTV.getText().toString());
+    }
+
+    /**
+     * Method which calls the super method on onCreateOptionsMenu to display the menu. Required
+     * so code to show the menu will not have to be repeated for each activity.
+     *
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -67,12 +106,22 @@ public class CalendarActivity extends MenuActivity
         return true;
     }
 
+    /**
+     * Method which calls the super method on onOptionsItemSelected to add functionality to the menu
+     * buttons without having to repeat the code for each activity.
+     *
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This method is used to set a DatePickerDialog on click listener on the date picker textview.
+     */
     public void setDatePickerTVListener(){
         datePickerTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +134,7 @@ public class CalendarActivity extends MenuActivity
                 cal.set(year, month, day);
                 DatePickerDialog dialog = new DatePickerDialog(
                         CalendarActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
                         dateSetListener,
                         year,month,day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -107,6 +156,10 @@ public class CalendarActivity extends MenuActivity
         };
     }
 
+    /**
+     * This method is used to set a TimePickerDialog on click listener on the start time picker
+     * textview.
+     */
     public void setStartTimePickerTVListener(){
         startTimePickerTV.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -117,7 +170,7 @@ public class CalendarActivity extends MenuActivity
                 cal.set(year, month, day, startHour, startMinute);
                 TimePickerDialog dialog = new TimePickerDialog(
                         CalendarActivity.this,
-                        android.R.style.Theme_DeviceDefault_Dialog,
+                        android.R.style.Theme_DeviceDefault_Dialog_MinWidth,
                         startTimeSetListener,
                         startHour, startMinute,
                         false);
@@ -144,6 +197,10 @@ public class CalendarActivity extends MenuActivity
         };
     }
 
+    /**
+     * This method is used to set a TimePickerDialog on click listener on the end time picker
+     * textview.
+     */
     public void setEndTimePickerTVListener(){
         endTimePickerTV.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -154,7 +211,7 @@ public class CalendarActivity extends MenuActivity
                 cal.set(year, month, day, endHour, endMinute);
                 TimePickerDialog dialog = new TimePickerDialog(
                         CalendarActivity.this,
-                        android.R.style.Theme_DeviceDefault_Dialog,
+                        android.R.style.Theme_DeviceDefault_Dialog_MinWidth,
                         endTimeSetListener,
                         endHour, endMinute,
                         false);
@@ -181,6 +238,11 @@ public class CalendarActivity extends MenuActivity
         };
     }
 
+    /**
+     * This method is used to validate if the end time picked by the user is after the start time.
+     *
+     * @return A boolean value
+     */
     public boolean validateTime(){
         String startTime;
         String endTime;
@@ -199,12 +261,18 @@ public class CalendarActivity extends MenuActivity
             return true;
     }
 
+    /**
+     * This method is used to add an event to the calendar.
+     * It is called when the user clicks on the [Add Event] button.
+     * @param v
+     */
     public void addEvent(View v){
         long calID = 1;
         long start;
         long end;
         boolean validStartEndTime = validateTime();
 
+        //Checks if a title is entered
         if(etTitle.getText().toString().equals("")){
             AlertDialog errorDialog = new AlertDialog.Builder(
                     CalendarActivity.this).create();
@@ -216,6 +284,7 @@ public class CalendarActivity extends MenuActivity
             });
             errorDialog.show();
         }
+        //Checks if a date was picked
         else if(datePickerTV.getText().toString().equals("")){
             AlertDialog errorDialog = new AlertDialog.Builder(
                     CalendarActivity.this).create();
@@ -227,7 +296,9 @@ public class CalendarActivity extends MenuActivity
             });
             errorDialog.show();
         }
-        else if(startTimePickerTV.getText().toString().equals("") || endTimePickerTV.getText().toString().equals("")){
+        //Checks if a start time and end time was picked
+        else if(startTimePickerTV.getText().toString().equals("") ||
+                endTimePickerTV.getText().toString().equals("")){
             AlertDialog errorDialog = new AlertDialog.Builder(
                     CalendarActivity.this).create();
             errorDialog.setTitle("Error!");
@@ -238,35 +309,37 @@ public class CalendarActivity extends MenuActivity
             });
             errorDialog.show();
         }
+        //Checks if the start time is before the end time
         else if(!validStartEndTime) {
             AlertDialog errorDialog = new AlertDialog.Builder(
                     this).create();
             errorDialog.setTitle("Error!");
-            errorDialog.setMessage("The end time cannot be earlier than the start time.");
+            errorDialog.setMessage("The end time cannot be earlier than the start time!");
             errorDialog.setButton("OK", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                 }
             });
             errorDialog.show();
         }
+        //Everything is selected, good to add the event to the calendar
         else{
             Calendar startTime = Calendar.getInstance();
-			startTime.set(year, month, day, startHour, startMinute);
-			start = startTime.getTimeInMillis();
-			Calendar endTime = Calendar.getInstance();
-			endTime.set(year, month, day, endHour, endMinute);
-			end = endTime.getTimeInMillis();
+            startTime.set(year, month, day, startHour, startMinute);
+            start = startTime.getTimeInMillis();
+            Calendar endTime = Calendar.getInstance();
+            endTime.set(year, month, day, endHour, endMinute);
+            end = endTime.getTimeInMillis();
 
-			ContentResolver cr = getContentResolver();
-			ContentValues values = new ContentValues();
-			values.put(CalendarContract.Events.DTSTART, start);
-			values.put(CalendarContract.Events.DTEND, end);
-			values.put(CalendarContract.Events.EVENT_TIMEZONE, TIMEZONE);
-			values.put(CalendarContract.Events.TITLE, etTitle.getText().toString());
-			values.put(CalendarContract.Events.CALENDAR_ID, calID);
-			Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+            ContentResolver cr = getContentResolver();
+            ContentValues values = new ContentValues();
+            values.put(CalendarContract.Events.DTSTART, start);
+            values.put(CalendarContract.Events.DTEND, end);
+            values.put(CalendarContract.Events.EVENT_TIMEZONE, TIMEZONE);
+            values.put(CalendarContract.Events.TITLE, etTitle.getText().toString());
+            values.put(CalendarContract.Events.CALENDAR_ID, calID);
+            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
 
             Toast.makeText(this, EVENTCREATED, Toast.LENGTH_SHORT).show();
         }
-	}
+    }
 }
